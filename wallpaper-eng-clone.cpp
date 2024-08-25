a0db5d58790d601f53573037866186fc001fe698
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2024-08-25 19:44:49 +0530

    Open box bettered

--- wallpaper-eng-clone.cpp
+++ wallpaper-eng-clone.cpp
@@ -1,18 +1,36 @@
 #ifndef UNICODE
 #define UNICODE
 #endif 
-
+#include "wallpaper-eng-clone.h"
+#include <commctrl.h>
 #include <iostream>
 #include <windows.h>
 #include <tchar.h>
+#include <shobjidl.h>  // for COM
 
+void opendabox(HWND hwnd);
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    HWND hButton;
+    static AppState* appState = nullptr;
     switch (uMsg)
     {
+    case WM_CREATE:
+        hButton = CreateWindowEx(
+            0, L"BUTTON", L"Open File",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            320, 10, 100, 25,
+            hwnd, (HMENU)1, ((LPCREATESTRUCT)lParam)->hInstance, NULL
+        );
+        appState = new AppState(L"appsettings.ini");
+        appState->RestoreWindowState(hwnd);
+        return 0;
     case WM_CLOSE:
         if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
         {
+            if (appState) {
+                appState->SaveWindowState(hwnd);
+            }
             DestroyWindow(hwnd);
             PostQuitMessage(0);
         }
@@ -20,6 +38,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             MessageBox(hwnd, L"You are a really funny person ngl", L"My app ofc", MB_OKCANCEL);
         }
         return 0;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) { // Button click
+            opendabox(hwnd);
+        }
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
@@ -47,7 +70,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     {
         std::cerr << "Failed to retrieve current wallpaper." << std::endl;
     }
-
+    
+   
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
@@ -60,7 +84,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     RegisterClass(&wc);
 
     // Create the window.
-    HWND hwnd = CreateWindowEx(
+     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
         L"Learn to Program Windows",    // Window text
@@ -75,12 +99,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL        // Additional application data
     );
 
+
     if (hwnd == NULL)
     {
         return 0;
     }
 
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     // Message loop
     MSG msg;
@@ -91,4 +117,36 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     }
 
     return 0;
+}
+
+void opendabox(HWND hwnd) {
+    HRESULT hr;
+    //Intializing the COM Library
+    hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);  // has to be null always
+    if (SUCCEEDED(hr)) {
+        IFileOpenDialog* pFileOpen;
+        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+        if (SUCCEEDED(hr)) {
+            // To show the box
+            hr = pFileOpen->Show(hwnd);
+
+            // to get file name from box
+            if (SUCCEEDED(hr)) {
+                IShellItem* pItem;
+                hr = pFileOpen->GetResult(&pItem);
+                if (SUCCEEDED(hr)) {
+                    PWSTR pszFilePath;
+                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+                    if (SUCCEEDED(hr)) {
+                        MessageBoxW(NULL, pszFilePath, L"File Path:", MB_OK);
+                        CoTaskMemFree(pszFilePath);
+                    }
+                    pItem->Release();
+                }
+                pFileOpen->Release();
+            }
+            CoUninitialize();
+        }
+    }
+
 }
--- wallpaper-eng-clone.h
+++ wallpaper-eng-clone.h
@@ -1,3 +1,40 @@
 #pragma once
 
-#include "resource.h"
+
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+class AppState {
+public:
+    AppState(const std::wstring& fileName) : m_fileName(fileName) {}
+
+    void SaveWindowState(HWND hwnd) {
+        RECT rect;
+        GetWindowRect(hwnd, &rect);
+
+        WritePrivateProfileString(L"Window", L"Left", std::to_wstring(rect.left).c_str(), m_fileName.c_str());
+        WritePrivateProfileString(L"Window", L"Top", std::to_wstring(rect.top).c_str(), m_fileName.c_str());
+        WritePrivateProfileString(L"Window", L"Width", std::to_wstring(rect.right - rect.left).c_str(), m_fileName.c_str());
+        WritePrivateProfileString(L"Window", L"Height", std::to_wstring(rect.bottom - rect.top).c_str(), m_fileName.c_str());
+    }
+
+    void RestoreWindowState(HWND hwnd) {
+        wchar_t szLeft[10], szTop[10], szWidth[10], szHeight[10];
+
+        GetPrivateProfileString(L"Window", L"Left", L"0", szLeft, sizeof(szLeft) / sizeof(wchar_t), m_fileName.c_str());
+        GetPrivateProfileString(L"Window", L"Top", L"0", szTop, sizeof(szTop) / sizeof(wchar_t), m_fileName.c_str());
+        GetPrivateProfileString(L"Window", L"Width", L"800", szWidth, sizeof(szWidth) / sizeof(wchar_t), m_fileName.c_str());
+        GetPrivateProfileString(L"Window", L"Height", L"600", szHeight, sizeof(szHeight) / sizeof(wchar_t), m_fileName.c_str());
+
+        int left = std::stoi(szLeft);
+        int top = std::stoi(szTop);
+        int width = std::stoi(szWidth);
+        int height = std::stoi(szHeight);
+
+        SetWindowPos(hwnd, NULL, left, top, width, height, SWP_NOZORDER);
+    }
+
+private:
+    std::wstring m_fileName;
+};
