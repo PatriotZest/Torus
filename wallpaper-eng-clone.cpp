1194ae09d71c3518e52ecbfc11049b602cb19a19
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2024-08-25 17:23:58 +0530

    Added close method

--- wallpaper-eng-clone.cpp
+++ wallpaper-eng-clone.cpp
@@ -10,14 +10,22 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
+    case WM_CLOSE:
+        if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+        {
+            DestroyWindow(hwnd);
+            PostQuitMessage(0);
+        }
+        else {
+            MessageBox(hwnd, L"You are a really funny person ngl", L"My app ofc", MB_OKCANCEL);
+        }
         return 0;
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
         // Here you can add code to paint your window
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
         EndPaint(hwnd, &ps);
     }
     return 0;
