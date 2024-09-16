916b944d2e6fdce12bcbeade24e9a4b1987c5264
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2024-09-16 14:17:59 +0530

    very final commit <- project done

--- wallpaper-eng-clone.cpp
+++ wallpaper-eng-clone.cpp
@@ -70,32 +70,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             hwnd, (HMENU)1, ((LPCREATESTRUCT)lParam)->hInstance, NULL
         );
 
-        if (hButton == NULL) {
-            MessageBox(hwnd, L"Failed to create 'Open File' button", L"Error", MB_OK | MB_ICONERROR);
-        }
-
-        hButton = CreateWindowEx(
-            0, L"BUTTON", L"Open Video",
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            0, 0, 100, 25, // x, y, width, height
-            hwnd, (HMENU)2, ((LPCREATESTRUCT)lParam)->hInstance, NULL
-        );
-
-        if (hButton == NULL) {
-            MessageBox(hwnd, L"Failed to create 'Open Video' button", L"Error", MB_OK | MB_ICONERROR);
-        }
+      
 
         return 0;
 
     case WM_CLOSE:
-        if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+        if (MessageBox(hwnd, L"Really quit?", L"Torus", MB_OKCANCEL) == IDOK)
         {
 
             DestroyWindow(hwnd);
             PostQuitMessage(0);
         }
         else {
-            MessageBox(hwnd, L"You are a really funny person ngl", L"My app ofc", MB_OKCANCEL);
+            MessageBox(hwnd, L"You are a really funny person ngl", L"Torus", MB_OKCANCEL);
         }
         return 0;
 
@@ -104,9 +91,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         if (LOWORD(wParam) == 1) { // Button click
             opendabox(hwnd);
         }
-        if (LOWORD(wParam) == 2) {
-            //this
-        }
+  
         return 0;
 
     default:
@@ -151,11 +136,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
         // Check for Control + Shift + Alt shortcut outside of the message handler
         if (isControlShiftAlt()) {
-            if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK) {
+            if (MessageBox(hwnd, L"Really quit?", L"Torus", MB_OKCANCEL) == IDOK) {
                 PostQuitMessage(0);
             }
             else {
-                MessageBox(hwnd, L"You are a really funny person ngl", L"My app ofc", MB_OK);
+                MessageBox(hwnd, L"You are a really funny person ngl", L"Torus", MB_OK);
             }
         }
     }
@@ -170,9 +155,7 @@ void loadImages(HWND hwnd, int frameCount) {
 
 
     HWND hButtonOpenFile = GetDlgItem(hwnd, 1);
-    HWND hButtonOpenVideo = GetDlgItem(hwnd, 2);
     ShowWindow(hButtonOpenFile, SW_HIDE);
-    ShowWindow(hButtonOpenVideo, SW_HIDE);
     while (true) {
         for (int i = 1; i < frameCount; i++) {
             if (i < 10) {
@@ -202,7 +185,6 @@ void loadImages(HWND hwnd, int frameCount) {
         }
     }
     ShowWindow(hButtonOpenFile, SW_SHOW);
-    ShowWindow(hButtonOpenVideo, SW_SHOW);
 
 
     // after ending part:
@@ -278,21 +260,20 @@ void sus(PWSTR videoPath, HWND hwnd) {
     std::string videoFilePath = ConvertPWSTRToString(videoPath);
     std::wstring wstr(videoFilePath.begin(), videoFilePath.end());
     OutputDebugString(wstr.c_str());
+
     std::wstring logPath = L"Frames";
     if (!DeleteFolderContents(logPath)) {
         MessageBox(hwnd, L"Failed to clear old frames.", L"Torus", MB_OKCANCEL | MB_ICONERROR);
         return;
     }
+
     // Define output frame path and FFmpeg path
     std::string outputFramePath = "Frames/frame_%04d.bmp";
     std::string ffmpegPath = "ffmpeg.exe";
-
-    // Construct the FFmpeg command
     std::string command = ffmpegPath + " -i \"" + videoFilePath + "\" \"" + outputFramePath + "\"";
 
     // Debug output for the command being executed
-    OutputDebugStringA(command.c_str());
-
+        
     // Execute the command and capture the result
     int result = std::system(command.c_str());
 
@@ -347,7 +328,6 @@ void opendabox(HWND hwnd) {
         }
     }
 }
-
 
 
 
